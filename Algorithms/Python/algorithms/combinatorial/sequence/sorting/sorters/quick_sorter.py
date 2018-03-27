4c9b44bcde14e8678fa355df677729743f0c94cd
CS-Labs <cseely@unm.edu>
2018-03-26 23:32:59 -0600

    update test cases.

--- Algorithms/Python/algorithms/combinatorial/sequence/sorting/sorters/quick_sorter.py
+++ Algorithms/Python/algorithms/combinatorial/sequence/sorting/sorters/quick_sorter.py
@@ -1,9 +1,10 @@
 # Author: Christian Seely
 
-def sort(t_aInput, t_bIterative = False, t_bConcurrent = False, t_fComparator = None):
-    if (all(isinstance(x,int) for x in t_aInput) or all(isinstance(x, float) for x in t_aInput) or
-            all(isinstance(x, str) for x in t_aInput)) or t_fComparator:
-        raise TypeError("Lists which do not contain solely integers, floats or strings must provide a custom comparator.")
+class Settings(object):
+    m_fComparator = None
+
+def sort(t_aInput, t_bIterative = False, t_bConcurrent = False, t_fComparator = lambda x,y: x <= y):
+    Settings.m_fComparator = t_fComparator
     return quicksort(t_aArr=t_aInput, t_iLo=0, t_iHi=len(t_aInput) - 1)
 
 def quicksort(t_aArr, t_iLo, t_iHi):
@@ -17,7 +18,7 @@ def partition(t_aArr, t_iLo, t_iHi):
     iPivot = t_aArr[t_iHi]
     iI = t_iLo - 1
     for iJ in range(t_iLo, t_iHi):
-        if t_aArr[iJ] <= iPivot:
+        if Settings.m_fComparator(t_aArr[iJ], iPivot):
             iI += 1
             t_aArr[iI], t_aArr[iJ] = t_aArr[iJ], t_aArr[iI] # Optimal swapping speed.
     t_aArr[iI+1],t_aArr[t_iHi] = t_aArr[t_iHi], t_aArr[iI+1] # Optimal swapping speed.
--- Algorithms/Python/tests/test_sorter.py
+++ Algorithms/Python/tests/test_sorter.py
@@ -2,32 +2,51 @@
 
 import unittest
 import random
+import string
 from itertools import chain
 
 
-from .context import sorter
-from .context import quick_sorter
+from context import sorter
+from context import quick_sorter
 
 
 class TestQuickSorter(unittest.TestCase):
 
-    def genRandIntArr(self, t_iN, t_iK):
-        return list(chain.from_iterable([random.sample(range(1, t_iK), t_iK - 1) for _ in range(t_iN)]))
+    def generateSamples(self):
+        return [
+            self.genRandIntArr(t_iN=10, t_iK=100),
+            self.genRandFloatArr(t_iN=10, t_iK=100, t_fStep=0.25),
+            self.genRandStringArr(t_iN=10, t_iK=100)
+        ]
 
-    def genRandFloatArr(self, t_iN, t_iK):
-        pass
+    def genSample(self, t_iN, aPool):
+        return list(chain.from_iterable([random.sample(aPool, len(aPool) - 1) for _ in range(t_iN)]))
+
+    def genRandIntArr(self, t_iN, t_iK):
+        aPool = range(t_iK)
+        return self.genSample(t_iN=t_iN, aPool=aPool)
+
+    def genRandFloatArr(self, t_iN, t_iK, t_fStep):
+        aPool = list((x / (1/t_fStep) for x in range(t_iK)))
+        return self.genSample(t_iN=t_iN, aPool=aPool)
 
     def genRandStringArr(self, t_iN, t_iK):
-        pass
+        sPool = string.ascii_letters + string.digits
+        aPool = [''.join(random.choice(sPool) for _ in range(10)) for _ in range(t_iK)]
+        return self.genSample(t_iN=t_iN, aPool=aPool)
 
     def testDirectSort(self):
-        aInput = self.genRandIntArr(t_iN=10,  t_iK=100)
-        self.assertEquals(quick_sorter.sort(t_aInput=aInput), sorted(aInput))
+        for aSample in self.generateSamples():
+            self.assertEquals(quick_sorter.sort(t_aInput=aSample), sorted(aSample))
 
     def testIndirectSort(self):
-        aInput = self.genRandIntArr(t_iN=10,  t_iK=100)
-        self.assertEquals(sorter.mySort(t_oAlg=sorter.SortingAlgo.QUICK_SORT, t_aInput=aInput), sorted(aInput))
+        for aSample in self.generateSamples():
+            print(len(aSample))
+            self.assertEquals(sorter.mySort(t_oAlg=sorter.SortingAlgo.QUICK_SORT, t_aInput=aSample), sorted(aSample))
 
+    def testCustomComparator(self):
+        for aSample in self.generateSamples():
+            self.assertEquals(quick_sorter.sort(t_aInput=aSample, t_fComparator=lambda x,y: x>=y), sorted(aSample, reverse=True))
 
 if __name__ == '__main__':
     unittest.main()
